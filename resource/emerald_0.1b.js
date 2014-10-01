1a714252b154d221ae285540fd57e23c1fda77f7
Ashbrooke <axeashbrooke@gmail.com>
2014-10-01 19:38:42 -0400

    Original website, ForeverAFK Subdomain, Stylesheet, and Javascript file added

---
+++ resource/emerald_0.1b.js
@@ -0,0 +1,11 @@
+function realmAyTeeVee(){
+    document.getElementById("content").innerHTML = "AyTeeVee";
+}
+
+function realmBlonkey(){
+    document.getElementById("content").innerHTML = "Blonkey";
+}
+
+function realmForeverAFK(){
+    document.getElementById("content").innerHTML = "ForeverAFK";
+}
