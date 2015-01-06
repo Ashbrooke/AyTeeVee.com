7b4023af8a22df60f4df3bcd52ef08fa03391828
Ashbrooke <axeashbrooke@gmail.com>
2015-01-06 14:36:45 -0500

    Added shenanigans

---
+++ shenans/phpshenans.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Example of Shenanigans</title>
+    </head>
+    <body>
+        <?php
+            echo "This is a PHP script.";
+        ?>
+    </body>
+</html>
---
+++ shenans/shenanigans.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <applet code="AshSites.class" width="500" height="230">
+            <param name="title0" value="ForeverAFK.net">
+            <param name="address0" value="http://foreverafk.net">
+            <param name="title1" value="Ash's Twitter">
+            <param name="address1" value="https://twitter.com/Ashbrooke_FAFK">
+        </applet>
+    </body>
+</html>
