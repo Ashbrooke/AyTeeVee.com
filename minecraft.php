727bd604d10d73975df05ae226f86edb6534e1fd
Ashbrooke <axeashbrooke@gmail.com>
2015-02-22 14:42:31 -0500

    Big changes to Minecraft
    Added huge amount of information to minecraft page.

--- minecraft.php
+++ minecraft.php
@@ -53,14 +53,39 @@ Description:    ForeverAFK brief about
        
         <article id="mainArticle">
         
-            <img src="https://mc.beastnode.com/index.php?r=status/81687.png"></img>
-            <h1>Click <a href="http://mc.foreverafk.net:8123/">here</a> to view the Dynmap.</h1></br>
+            <h1>You can connect to our Minecraft server using the ip: mc.ForeverAFK.net</h1>
+            <h1>Click <a href="http://mc.foreverafk.net:8123/">here</a> to view the live map.</h1></br>
             </br>
             <h1>Minecraft Server</h1>
-            To connect to our Minecraft server use the IP: mc.ForeverAFK.net</br>
+            <h2>Server Information</h2>
+            The ForeverAFK Minecraft server is a multiworld SMP server with available plot protection. There are two main worlds: Meilin and Sigby. Meilin is the world you will first spawn in. The largest majority of this world is free for everyone to build in. This leaves players builds vulnerable to thieves and griefers. Once a player has been on the server for 2 hours they gain access to Sigby. Unlike Meilin, players will not be able to build anywhere in Sigby unless they own a specific plot of land that is protected from other players.
             </br>
-            This is a public server, so feel free to connect with your friends. The server hosts two worlds: Meilin and Sigby. Meilin is a completely public world, and with the exception of spawn there are no protections. Sigby has protected plots that can be purchased using in-game items or Loyalty Points. If you have any questions please ask a mod!
+            <h2>Server Rules</h2>
+            <ol>
+                <li>Asking for Mod will get you kicked. Asking again will get you banned. Being a Mod is a responsibility, not an honor. It will also tell me that you haven't even bothered to read the rules.</li>
+                <li>Client mods are allowed only if they do not alter gameplay and are in good spirit with the game. For example, maps and graphical mods are allowed, but flying and hack clients are not. When in doubt ask a Mod.</li>
+                <li>Any attempt to cheat/hack will be swiftly rewarded with a permaban. This includes exploiting bugs on the server that obviously shouldn't be there. Please report these issues to a Mod immediately.</li>
+                <li>There is absolutely NO GRIEFING in Sigby.</li>
+                <li>Meilin is unprotected and free-for-all. Protect yourself and your things at all times.</li>
+                <li>Use common sense and be respectful. We are all here to have fun, and the Mods are here to help make the server a good place for everyone.</li>
+            </ol>
             </br>
+            <h2>Server Policy</h2>
+            <h3>Ranks and Promotion</h3>
+            <ol>
+                <li>[AP] - Apprentice - This is the default rank. Players may only access Meilin.</li>
+                <li>[JM] - Journeyman - After two hours playtime an Apprentice is promoted to Journeyman. They can now access Sigby and build in regions they have been given specific permission for.</li>
+                <li>[MA] - Master - When a player purchases their first plot of land in Sigby they earn the rank of Master. Masters have the ability to add other Journeyman players to their build area. If you are looking for a protected plot and can't find a mod, ask a master.</li>
+                <li>(Various titles) - Guru - This is our VIP rank. We honor our best friends and special guests with this rank.</li>
+                <li>Mod - Moderator - Mods have taken on the responsibility of helping other players with any problems. They are giving up their game-time to help others, so please treat them respectfully.</li>
+            </ol>
+            </br>
+            <h3>Multiworld Architecture</h3>
+            The server is running two overworlds: A protected world, Sigby, and a free-for-all world, Meilin. Meilin includes access to a Nether world, as well as an End world. Portals in Sigby have been disabled, and lead nowhere. </br>
+            From time to time the server will be migrating into a new Meilin world. During this time there will be 3 active overworlds. It is strongly suggested that you move all of your valuables into the new Meilin (or better yet to Sigby) so that you aren't missing anything when the migration is completed. This includes anything in the Nether and End worlds as well.
+            <h3>Plot Ownership</h3>
+            Masters and Mods own the largest portions of land in Sigby. At their discretion they may sublet portions to Journeyman. Any person who has been inactive in their region is subject to eviction, with a warning period of no less than 2 weeks.</br>
+            Crafterville is a plotgrid in Meilin that is a great place to prove your building skills to landlords. While these regions are protected they are not permanent, and therefore being offered a plot in Sigby is essential. Masters and Mods will look here to find builders worthy of building on plots in Sigby.
             </br>
             <h1>Mumble VOIP</h1>
             We also host a Mumble chat server if you want to talk voice-to-voice. You can usually find Erk and Ash in there when they are on.
@@ -71,7 +96,6 @@ Description:    ForeverAFK brief about
             You can download the Mumble client <a href="http://www.mumble.com/mumble-download.php" target="_blank" class="inarticle">here</a>.
             
         </article>
-        
         <div id="footer">
             <a href="about.php">About</a> | 
             <a href="contact.php">Contact</a>
